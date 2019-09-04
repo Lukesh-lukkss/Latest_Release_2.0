1ced9dab08c777d28ae801d1f22bb6a1c583831d
Lukesh <lukeshlukkss@gmail.com>
2019-09-04 14:44:29 +0530

    modified clonned content and pushed to remote repo

--- remote_repo.py
+++ remote_repo.py
@@ -2,5 +2,6 @@ o#this code added in remote repo
 #this code is added in local repo before merge
 #adding code before merging
 #addigng come code in local repo
-#adding in remote repo 
+#adding in remote repo
+#this is ADDED after clonning and has to be merged and pushed to remote repo 
 >>>>>>> 7d08108cca9fefa7e47f5f1f9c298d26c464fc0e
